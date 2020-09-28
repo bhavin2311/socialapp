411b22e3c69a8daa2be7e688f3c2e9a76013b96f
bhavin2311 <40229713+bhavin2311@users.noreply.github.com>
2020-09-28 14:15:17 +0530

    Updates

---
+++ precache-manifest.c7090688d73f02f00ea7fc798b5f5b99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ef02a8ea56fe27dddf2a3a57f79258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "501b73dfac3fc8a5dd1d",
+    "url": "/static/css/main.06815a4a.chunk.css"
+  },
+  {
+    "revision": "0929c5807a68c6079b1b",
+    "url": "/static/js/2.aaf9bb7a.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.aaf9bb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501b73dfac3fc8a5dd1d",
+    "url": "/static/js/main.fe823ff3.chunk.js"
+  },
+  {
+    "revision": "6c7c0d0950d4c9ca2e51",
+    "url": "/static/js/runtime-main.414b2360.js"
+  },
+  {
+    "revision": "6e1267d9d946b0236cdf6ffd02890894",
+    "url": "/static/media/favicon.6e1267d9.ico"
+  }
+]);
